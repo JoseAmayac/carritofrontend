5315b9280f8c52d0755253a111087cf7d8e905fe
José Amaya <jlacarrascal2684@gmail.com>
2019-12-12 02:10:14 -0500

    s al protocolo

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -55,7 +55,7 @@ export class TokenService {
       const payload = this.payload(token);
       if(payload)
       {
-        return Object.values(this.iss).indexOf(payload.iss) > -1 ? true : false;
+        return Object.values(this.iss).indexOf(payload.iss+"s") > -1 ? true : false;
       }
     }
     return false;
