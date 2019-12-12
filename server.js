06c3d0797549e48b47100a490aeaeb526277a4b4
José Amaya <jlacarrascal2684@gmail.com>
2019-12-12 01:39:09 -0500

    Ahora sí

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/carritocomprasfrontend'));
+app.use(express.static(__dirname + '/dist/carritocomprasFrontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/carritocomprasfrontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/carritocomprasFrontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
